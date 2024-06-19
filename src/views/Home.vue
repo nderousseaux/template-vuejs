c3c7d5c3dac335121b50d6fafca93fe12e64c912
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2024-06-19 16:37:11 +0200

    draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="home-view">
-    Home view
-  </div>
+  <n-space id="home-view"
+    vertical
+    size="large"
+  >
+    <p>Home View</p>
+    <p>Home View</p>
+    <p>Home View</p>
+
+  </n-space>
 </template>
 
 <script>
+import { NSpace } from 'naive-ui'
+
 export default {
   name: 'HomeView',
+
+  components: {
+    NSpace
+  }
 }
 </script>
